68b980ca7f3a5e498c51ff6f7a76823f952e35ea
Vich <yaaska@gmail.com>
2024-11-05 13:05:55 +0300

    Added Variables

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function calculateTotalTarget(startDate, endDate, totalAnnualTarget, daysToExclude = [5]) {
-    
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+  
+    let daysExcludingFridays = [];
+    let daysWorkedExcludingFridays = [];
+    let monthlyTargets = [];
+    let totalTarget = 0;
 }
